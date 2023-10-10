061a0891970ac595826d574ef7ae840bb1fc95a1
DezesTheOne <hedvika.reichlova@gmail.com>
2023-10-10 14:46:05 +0200

    zaobleni_rohu

--- hra.js
+++ hra.js
@@ -1,11 +1,38 @@
+let HrajiciHrac = 'cerny_krizek';
 const policka = document.querySelectorAll('.cell');
-let symbol = 'cerny_krizek';
+const playerElement = document.querySelector('player');
 
 policka.forEach((pole) => {
-  pole.addEventListener('click', () => {
+  const clickHandler = () => {
     if (pole.textContent === '') {
-      pole.classList.add(symbol);
-      symbol = symbol === 'cerny_krizek' ? 'cerne_kolecko' : 'cerny_krizek';
+      pole.classList.add(HrajiciHrac);
+      HrajiciHrac = HrajiciHrac === 'cerny_krizek' ? 'cerne_kolecko' : 'cerny_krizek';
+      pole.removeEventListener('click', clickHandler);
     }
-  });
+  };
+
+  pole.addEventListener('click', clickHandler);
 });
+
+
+
+
+// let hracKrizek = "podklady/cross_white.svg";
+// let hracKolecko = "podklady/circle_white.svg";
+
+// let aktualniHrac = hracKrizek;
+
+// function aktualizovatHraceNaRade() {
+//   let playerElement = document.querySelector("player");
+//   playerElement.src = aktualniHrac;
+// }
+
+// function prepnoutHrace() {
+//   if (aktualniHrac === hracKrizek) {
+//     aktualniHrac = hracKolecko;
+//   } else {
+//     aktualniHrac = hracKrizek;
+//   }
+// }
+
+// prepnoutHrace();
