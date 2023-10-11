0ca5cad80c2cfae47fe7a4aaea3974baec956ccf
DezesTheOne <hedvika.reichlova@gmail.com>
2023-10-11 10:07:41 +0200

    oprava_podminka_kdo_hraje

--- hra.js
+++ hra.js
@@ -2,7 +2,7 @@
 
 let HrajiciHrac = 'cerny_krizek';
 const policka = document.querySelectorAll('.cell');
-const playerElement = document.querySelector('player');
+const playerElement = document.querySelector('.player');
 
 policka.forEach((pole) => {
   const clickHandler = () => {
@@ -11,14 +11,15 @@ policka.forEach((pole) => {
       HrajiciHrac =
         HrajiciHrac === 'cerny_krizek' ? 'cerne_kolecko' : 'cerny_krizek';
       pole.removeEventListener('click', clickHandler);
+      if (HrajiciHrac === 'cerny_krizek') {
+        playerElement.src = 'podklady/cross_white.svg';
+      } else {
+        playerElement.src = 'podklady/circle_white.svg';
+      };
     }
   };
 
   pole.addEventListener('click', clickHandler);
 });
 
-if (HrajiciHrac === 'cerny_krizek') {
-  playerElement.src = 'podklady/circle_white.svg';
-} else {
-  playerElement.src = 'podklady/cross_white.svg';
-};
+
