3522dfe3de72f46bd0958902f2934f218772dc8f
DezesTheOne <hedvika.reichlova@gmail.com>
2023-10-13 18:40:40 +0200

    pridani_funkce_findWinner

--- hra.js
+++ hra.js
@@ -1,8 +1,15 @@
-// import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let HrajiciHrac = 'cerny_krizek';
 const policka = document.querySelectorAll('.cell');
 const playerElement = document.querySelector('.player');
+let herniPole = [];
+
+const ctverecky = document.querySelectorAll('.cell');
+ctverecky.forEach((ctverecek) => {
+  let hodnota = '_';
+  herniPole.push(hodnota);
+});
 
 policka.forEach((pole) => {
   const clickHandler = () => {
@@ -15,11 +22,30 @@ policka.forEach((pole) => {
         playerElement.src = 'podklady/cross_white.svg';
       } else {
         playerElement.src = 'podklady/circle_white.svg';
-      };
+      }
+    }
+    herniPole = [];
+    const ctverecky = document.querySelectorAll('.cell');
+    ctverecky.forEach((ctverecek) => {
+      let hodnota = ctverecek.classList.value;
+      // console.log(hodnota);
+      if (hodnota === 'cell') {
+        hodnota = '_';
+      } else if (hodnota === 'cell cerny_krizek') {
+        hodnota = 'x';
+      } else {
+        hodnota = 'o';
+      }
+      herniPole.push(hodnota);
+    });
+
+    const vitez = findWinner(herniPole);
+    if (vitez === 'o' || vitez === 'x') {
+      setTimeout(() => {
+        alert(`Vyhrál hráč se symbolem ${vitez}.`);
+        location.reload();
+      }, 0);
     }
   };
-
   pole.addEventListener('click', clickHandler);
 });
-
-
