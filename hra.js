bd5ceed6b7de4fff17318de1513cc1b4a74ced6c
DezesTheOne <hedvika.reichlova@gmail.com>
2023-10-24 10:25:54 +0200

    umela_inteligence

--- hra.js
+++ hra.js
@@ -1,6 +1,7 @@
+
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-let HrajiciHrac = 'cerny_krizek';
+let HrajiciHrac = 'cerne_kolecko';
 const policka = document.querySelectorAll('.cell');
 const playerElement = document.querySelector('.player');
 let herniPole = [];
@@ -12,39 +13,58 @@ ctverecky.forEach((ctverecek) => {
 });
 
 policka.forEach((pole) => {
-  const clickHandler = () => {
+  const clickHandler = async () => {
     if (pole.textContent === '') {
       pole.classList.add(HrajiciHrac);
       HrajiciHrac =
-        HrajiciHrac === 'cerny_krizek' ? 'cerne_kolecko' : 'cerny_krizek';
+        HrajiciHrac === 'cerne_kolecko' ? 'cerny_krizek' : 'cerne_kolecko';
       pole.removeEventListener('click', clickHandler);
       if (HrajiciHrac === 'cerny_krizek') {
         playerElement.src = 'podklady/cross_white.svg';
       } else {
         playerElement.src = 'podklady/circle_white.svg';
       }
-    }
-    herniPole = [];
-    const ctverecky = document.querySelectorAll('.cell');
-    ctverecky.forEach((ctverecek) => {
-      let hodnota = ctverecek.classList.value;
-      // console.log(hodnota);
-      if (hodnota === 'cell') {
-        hodnota = '_';
-      } else if (hodnota === 'cell cerny_krizek') {
-        hodnota = 'x';
-      } else {
-        hodnota = 'o';
-      }
-      herniPole.push(hodnota);
-    });
 
-    const vitez = findWinner(herniPole);
-    if (vitez === 'o' || vitez === 'x') {
-      setTimeout(() => {
-        alert(`Vyhrál hráč se symbolem ${vitez}.`);
-        location.reload();
-      }, 0);
+      herniPole = [];
+      const ctverecky = document.querySelectorAll('.cell');
+      ctverecky.forEach((ctverecek) => {
+        let hodnota = ctverecek.classList.value;
+        if (hodnota === 'cell') {
+          hodnota = '_';
+        } else if (hodnota === 'cell cerny_krizek') {
+          hodnota = 'x';
+        } else {
+          hodnota = 'o';
+        }
+        herniPole.push(hodnota);
+      });
+
+      const vitez = findWinner(herniPole);
+      if (vitez === 'o' || vitez === 'x') {
+        setTimeout(() => {
+          alert(`Vyhrál hráč se symbolem ${vitez}.`);
+          location.reload();
+        }, 0);
+      } 
+      
+      
+      else if (HrajiciHrac === 'cerny_krizek') {
+        const response = await fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            board: herniPole,
+            player: 'x',
+          }),
+        });
+        const data = await response.json();
+        const { x, y } = data.position;
+        const index = x + y * 10;
+        const field = policka[index];
+        field.click();
+      }
     }
   };
   pole.addEventListener('click', clickHandler);
